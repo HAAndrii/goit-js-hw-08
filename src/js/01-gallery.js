64562faf371cd3c3bc2f2cc34f81a228a692e442
Andrii Kharchenko <107865478+HAAndrii@users.noreply.github.com>
2023-03-12 12:47:36 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+createGallery(galleryItems);
+
+function createGallery(items) {
+    const arr = items.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    })
+    galleryEl.insertAdjacentHTML('afterbegin', arr.join(''));
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+//console.log(galleryItems);
